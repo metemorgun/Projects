79c1fbd42478247549a153be8608273c83012b98
Metehan Morgün <109622990+metemorgun@users.noreply.github.com>
2023-08-21 12:08:05 +0300

    Add files via upload

---
+++ ERC1155tokendeploy.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Mete1155NFT is ERC1155, Ownable {
+    string public name;
+    string public symbol;
+
+    mapping(uint => string) public tokenURI;
+
+    constructor(string memory _baseURI) ERC1155(_baseURI) {
+        name = "Mete1155NFT";
+        symbol = "MT1155";
+    }
+
+    function mint(address _to, uint _id, uint _amount) external onlyOwner {
+        _mint(_to, _id, _amount, "");
+    }
+
+    function mintBatch(address _to, uint[] memory _ids, uint[] memory _amounts) external onlyOwner {
+        _mintBatch(_to, _ids, _amounts, "");
+    }
+
+    function burn(uint _id, uint _amount) external {
+        _burn(msg.sender, _id, _amount);
+    }
+
+    function burnBatch(uint[] memory _ids, uint[] memory _amounts) external {
+        _burnBatch(msg.sender, _ids, _amounts);
+    }
+
+    function burnForMint(
+        address _from,
+        uint[] memory _burnIds,
+        uint[] memory _burnAmounts,
+        uint[] memory _mintIds,
+        uint[] memory _mintAmounts
+    ) external onlyOwner {
+        _burnBatch(_from, _burnIds, _burnAmounts);
+        _mintBatch(_from, _mintIds, _mintAmounts, "");
+    }
+
+    function setURI(uint _id, string memory _uri) external onlyOwner {
+        tokenURI[_id] = _uri;
+        emit URI(_uri, _id);
+    }
+
+    function uri(uint _id) public override view returns (string memory) {
+        return tokenURI[_id];
+    }
+}
